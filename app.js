81e7d9fe74c7e02237fcce9caa7d1b40850bd625
Olu Adeyemo <theoafactor@gmail.com>
2026-08-21 14:23:24 +0100

    Added database

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require("express");
+require('dotenv').config()
+
+const { MongoClient } = require("mongodb");
+
+const url = process.env.DB_CONNECT;
+
+const mongo_client = new MongoClient(url);
+
+
 
 // create a simple server
 const server = express();
@@ -8,7 +17,7 @@ server.use(express.json());
 // create the routes 
 server.get("/search", (request, response) => {
 
-    response.send({
+    response.status(200).send({
         message: "You are on the about route",
         data: {
             username: "James",
@@ -18,15 +27,26 @@ server.get("/search", (request, response) => {
 })
 
  
-server.post("/login", (request, response) => {
+server.post("/login", async (request, response) => {
 
   
 
     let fullname = request.body.fullname;
+    let username = request.body.username;
+    let password = request.body.password;
+
+    await mongo_client.connect();
     
+    let result = await mongo_client.db("backend-db").collection("users").insertOne({
+        fullname: fullname, 
+        username: username,
+        password: password
+    });
+
+    console.log(result)
 
 
-    response.send({
+    response.status(200).send({
         message: "User logged in",
         data: {
             fullname: fullname
@@ -37,4 +57,4 @@ server.post("/login", (request, response) => {
 
 
 // make the server listen for request
-server.listen(3000, () => console.log("Server is listening on 3000"))
+server.listen(process.env.PORT, () => console.log(`Server is listening on http://${process.env.HOSTNAME}:${process.env.PORT}`))
---
+++ test.js
@@ -0,0 +1,12 @@
+function getUser(firstname, lastname){
+    return firstname;
+}
+
+
+let promise = new Promise((resolve, reject) => {
+
+    let is_user = 1;
+    if()
+
+
+}) 
